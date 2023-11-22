d8310acf36bb65c056ec575f0b0dfe0dc32032c7
rdal28 <148552071+rdal28@users.noreply.github.com>
2023-11-22 16:54:05 +0100

    Add files via upload

---
+++ TrajetSimple.h
@@ -0,0 +1,69 @@
+/*************************************************************************
+                           TrajetSimple  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <TrajetSimple> (fichier TrajetSimple.h) ----------------
+#if ! defined ( TRAJETSIMPLE_H )
+#define TRAJETSIMPLE_H
+
+#include "Trajet.h"
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <TrajetSimple>
+//
+//
+//------------------------------------------------------------------------
+
+class TrajetSimple : public Trajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    void Afficher (  );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+
+    TrajetSimple ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    ~TrajetSimple ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+    char moyenTransport[50];
+    
+};
+
+//-------------------------------- Autres définitions dépendantes de <TrajetSimple>
+
+#endif // TRAJETSIMPLE_H
+
+
+
