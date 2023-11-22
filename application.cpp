a7a404026417e943d9240185383ff2b1df1c098b
Rayan Hanader <rayan.hanader@gmail.com>
2023-11-22 16:53:07 +0100

    wewe

--- application.cpp
+++ application.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+using namespace std;
+#include <cstring>
+#include "TrajetSimple.h"
+#include "TrajetCompose.h"
+#include "Catalogue.h"
+
+int main()
+{   
+    cout << "Initi" << endl;
+    cout << "Bienvenue dans l'application de gestion de trajets" << endl;
+    Catalogue catalogue;
+
+}
