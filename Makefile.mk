63d1e8ce34c7dbb7aa81ca85617394fea5118b3f
Djalil Chikhi <djalilchiki@gmail.com>
2023-11-29 14:26:44 +0100

    make file correction

--- Makefile.mk
+++ Makefile.mk
@@ -1,6 +1,29 @@
-exe : application.cpp, Catalogue.cpp, TrajetSimple.cpp, Trajet.cpp
-	g++ -c application.cpp
-	g++ -c Catalogue.cpp
-	g++ -c TrajetSimple.cpp
-	g++ -c Trajet.cpp
-	g++ -o exe application.o Catalogue.o TrajetSimple.o Trajet.o
+# List of source files
+SRCS = application.cpp Catalogue.cpp TrajetSimple.cpp Trajet.cpp
+
+# List of object files (generated during compilation)
+OBJS = $(SRCS:.cpp=.o)
+
+# Compiler
+CC = g++
+
+# Compiler flags
+CFLAGS = -c -Wall
+
+# Linker flags
+LFLAGS = 
+
+# Executable name
+EXECUTABLE = exe
+
+# Target: build the executable
+$(EXECUTABLE): $(OBJS)
+	$(CC) $(LFLAGS) $(OBJS) -o $(EXECUTABLE)
+
+# Target: compile source files to object files
+%.o: %.cpp
+	$(CC) $(CFLAGS) $< -o $@
+
+# Target: clean up generated files
+clean:
+	rm -f $(OBJS) $(EXECUTABLE)
